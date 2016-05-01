91d18ba5154e132083848f5b10dc495039c3d077
anuragpeshne <anurag.peshne@gmail.com>
2016-05-01 22:46:22 +0530

    init ox-article

---
+++ ox-article.el
@@ -0,0 +1,20 @@
+;;; ox-article.el --- Article Back-End for Org Export Engine -*- lexical-binding: t; -*-
+
+;; Author: Anurag Peshne <anurag.peshne AT gmail DOT com>
+;; Keywords: org, blog, article
+
+;;; Commentary:
+;;
+;; This library implements a simple article back-end, derived from HTML back-end.
+;; Used for exporting org file as article in blog.
+
+;;; Code:
+
+(org-export-define-backend 'article)
+(provide 'ox-article)
+
+;; Local variables:
+;; generated-autoload-file:
+;; End:
+
+;;; ox-article.el ends here
