5a67e051f93dc589c3b203c30c42376fcbfd64a1
Anurag Peshne <anurag.peshne@gmail.com>
2016-07-20 18:20:59 +0530

    alpha working

--- ox-article.el
+++ ox-article.el
@@ -9,8 +9,43 @@
 ;; Used for exporting org file as article in blog.
 
 ;;; Code:
+(require 'ox-html)
 
-(org-export-define-backend 'article)
+(org-export-define-derived-backend 'blog-html 'html
+                                   :translate-alist '((template . blog-html-template)))
+
+(defun blog-html-template (contents info)
+  (concat
+   "<!DOCTYPE html>\n"
+   (format "<html lang=\"%s\">\n" (plist-get info :language))
+   "<head>\n"
+   (format "<meta charset=\"%s\">\n"
+           (coding-system-get org-html-coding-system 'mime-charset))
+   (format "<title>%s</title>\n"
+           (org-export-data (or (plist-get info :title) "") info))
+   (format "<meta name=\"author\" content=\"%s\">\n"
+           (org-export-data (plist-get info :author) info))
+   "</head>\n"
+   "<body>\n"
+   (format "<h1 class=\"title\">%s</h1>\n"
+           (org-export-data (or (plist-get info :title) "") info))
+   contents
+   "</body>\n"
+   "</html>\n"))
+
+;;; End-user functions
+;;;###autoload
+(defun org-blog-export-to-html
+    (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer to a blog article."
+  (interactive)
+  (let* ((extension (concat "." org-html-extension))
+         (file (org-export-output-file-name extension subtreep))
+         (org-export-coding-system org-html-coding-system))
+    ;; export to html use article
+    (org-export-to-file
+        'blog-html file subtreep visible-only body-only ext-plist)))
+
 (provide 'ox-article)
 
 ;; Local variables:
